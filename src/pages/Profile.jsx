8c241b8cdabd9e9cbfdb2418e422b3c49528adb5
TobiA34 <36420903+TobiA34@users.noreply.github.com>
2023-12-18 15:11:52 +0000

    Implement log out on profile screen

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,37 @@
 import { db, getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function Profile() {
+  const [user, setUser] = useState(null);
 
-   const [user,setUser] = useState(null)
-   const auth = getAuth();
+  const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
+  const navigate = useNavigate();
 
-useEffect(() => {
- setUser(auth.currentUser)
- },[])
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
 
-  return user ? <h1>{user.displayName}</h1> : "not logged in";
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
