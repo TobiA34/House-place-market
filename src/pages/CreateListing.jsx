af2dde6ab5112083a3ef0cb00737241080bd3b7a
TobiA34 <36420903+TobiA34@users.noreply.github.com>
2024-01-15 10:32:41 +0000

    - Uploaded multiple images to firebase storage

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -72,48 +72,94 @@ function CreateListing() {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-      console.log(formData)
-      setLoading(true)
-      if (discountedPrice >= regularPrice) {
-        setLoading(false)
-        toast.error('Discounted price needs to be less than regular price')
+    console.log(formData);
+    setLoading(true);
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price needs to be less than regular price");
+    }
+
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Max 6 images");
+      return;
+    }
+
+    let geolocation = {};
+    let location;
+
+    if (geolocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : data.results[0].formatted_address;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
       }
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+      location = address;
+    }
+    // Store images in firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, "images/" + fileName);
 
-      if(images.length > 6) {
-        setLoading(false)
-        toast.error('Max 6 images')
-        return
-      }
-
-      let geolocation = {}
-      let location
-
-      if(geolocationEnabled) {
-        const response = await fetch(
-          `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error)
+           },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+               resolve(downloadURL)
+            });
+          }
         );
-        const data = await response.json()
-        console.log(data)
-        geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
-        geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
-
-        location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0].formatted_address
-
-        if(location === undefined || location.includes('undefined')) {
-          setLoading(false)
-          toast.error('Please enter a correct address')
-          return
-        }
-      } else {
-        geolocation.lat = latitude
-        geolocation.lng = longitude
-        location = address
-      }
-      setLoading(false)
+      });
     };
 
-     
- 
+    const imgURLS = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false)
+      toast.error('Images not uploaded')
+      return
+    })
+    console.log(imgURLS)
+    setLoading(false);
+  };
+
   const onMutate = (e) => {
     let boolean = null;
 
