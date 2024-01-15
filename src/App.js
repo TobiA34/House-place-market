a50539b22171502f2e2cdd9d783277575738cfaa
TobiA34 <36420903+TobiA34@users.noreply.github.com>
2024-01-15 09:17:36 +0000

    - Added login and register feature - Got item from firebase for explore page - Created profile page and offers page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Explore from './pages/Explore'
 import Offers from "./pages/Offers";
+import Category from "./pages/Category";
+
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import { ToastContainer } from "react-toastify";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -16,12 +19,15 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />}></Route>
           <Route path="/offers" element={<Offers />}></Route>
-           <Route path="/profile" element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile/>}/>
-           </Route>
+          <Route path="/category/:categoryName" element={<Category />}></Route>
+
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/create-listing" element={<CreateListing/>}/>
         </Routes>
         <Navbar />
       </Router>
