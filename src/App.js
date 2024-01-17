2d71bb76f8479a950050f2143604bb284365bfc4
TobiA34 <36420903+TobiA34@users.noreply.github.com>
2024-01-17 08:38:05 +0000

    Created contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Explore from './pages/Explore'
+import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Category from "./pages/Category";
 
@@ -12,6 +12,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -29,7 +30,11 @@ function App() {
           <Route path="/sign-up" element={<SignUp />}></Route>
           <Route path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/contact/:landordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
